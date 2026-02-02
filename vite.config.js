249a12b03ab49ae0955e7f94f85b2315dba3c2df
Shishirthakur2416 <samadkhan040204@gmail.com>
2026-02-02 14:00:13 +0530

    reloading isssue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://e-chat-production.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/socket.io': {
+        target: 'https://e-chat-production.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    }
+  },
+  define: {
+    global: {}
+  }
 })
